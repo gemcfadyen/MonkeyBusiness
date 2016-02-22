691524d4c20cc8a197c9dcf3f7deb9e2b3ca5a20
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-02-22 11:34:03 +0000

    Server can listen for a client to connect and parse a http request

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -1,10 +1,17 @@
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 public class HttpServer {
     private String host;
     private int port;
+    private HttpServerSocket serverSocket;
+    private HttpRequestParser httpRequestParser;
 
-    public HttpServer(String host, int port) {
+    public HttpServer(String host, int port, HttpServerSocket serverSocket, HttpRequestParser httpRequestParser) {
         this.host = host;
         this.port = port;
+        this.serverSocket = serverSocket;
+        this.httpRequestParser = httpRequestParser;
     }
 
     public String getHost() {
@@ -14,4 +21,63 @@ public class HttpServer {
     public int getPort() {
         return port;
     }
+
+    public void start() {
+        HttpSocket client = serverSocket.accept();
+        HttpRequest httpRequest = httpRequestParser.parse(client.getRawHttpRequest());
+    }
+}
+
+interface HttpServerSocket {
+    HttpSocket accept();
+}
+
+class ServerSocketSpy implements HttpServerSocket {
+    private boolean isAcceptingRequests = false;
+
+    @Override
+    public HttpSocket accept() {
+        isAcceptingRequests = true;
+        return new FakeClient();
+    }
+
+    public boolean isAcceptingRequests() {
+        return isAcceptingRequests;
+    }
+}
+
+class FakeClient implements HttpSocket {
+
+    @Override
+    public InputStream getRawHttpRequest() {
+        return new ByteArrayInputStream("An Http Request".getBytes());
+    }
+}
+
+interface HttpSocket {
+    InputStream getRawHttpRequest();
+}
+
+interface HttpRequestParser {
+
+    HttpRequest parse(InputStream inputStream);
+
+}
+
+class HttpRequestParserSpy implements HttpRequestParser {
+    private boolean hasParsedRequest = false;
+
+    @Override
+    public HttpRequest parse(InputStream inputStream) {
+        hasParsedRequest = true;
+        return null;
+    }
+
+    public boolean hasParsedRequest() {
+        return hasParsedRequest;
+    }
+}
+
+class HttpRequest {
+
 }
--- src/test/java/HttpServerTest.java
+++ src/test/java/HttpServerTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
@@ -5,17 +6,38 @@ import static org.junit.Assert.assertThat;
 
 public class HttpServerTest {
 
+    private ServerSocketSpy serverSocketSpy;
+    private HttpRequestParserSpy httpRequestParserSpy;
+    private HttpServer httpServer;
+
+    @Before
+    public void setUp() throws Exception {
+        serverSocketSpy = new ServerSocketSpy();
+        httpRequestParserSpy = new HttpRequestParserSpy();
+        httpServer = new HttpServer("localhost", 8080, serverSocketSpy, httpRequestParserSpy);
+    }
+
     @Test
     public void serverHasAHost() {
-        HttpServer httpServer = new HttpServer("localhost", 1);
-
         assertThat(httpServer.getHost(), is("localhost"));
     }
 
     @Test
     public void serverHasAPort() {
-        HttpServer httpServer = new HttpServer("localhost", 8080);
-
         assertThat(httpServer.getPort(), is(8080));
+    }
+
+    @Test
+    public void whenServerIsStartedItAcceptsClientRequests() {
+        httpServer.start();
+
+        assertThat(serverSocketSpy.isAcceptingRequests(), is(true));
+    }
+
+    @Test
+    public void serverAcceptsClientRequest() {
+        httpServer.start();
+
+        assertThat(httpRequestParserSpy.hasParsedRequest(), is(true));
     }
 }
