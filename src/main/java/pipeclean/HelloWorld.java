04b8cb4c725dfb1a6253047b2aa19a253efea420
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-02-22 09:28:56 +0000

    Add the ability to run the main method from the gradle file

--- src/main/java/pipeclean/HelloWorld.java
+++ src/main/java/pipeclean/HelloWorld.java
@@ -4,4 +4,10 @@ public class HelloWorld {
     public String greeting() {
         return "Hiiii";
     }
+
+    public static void main(String... args) {
+        HelloWorld hi = new HelloWorld();
+        System.out.println(hi.greeting());
+    }
+
 }
