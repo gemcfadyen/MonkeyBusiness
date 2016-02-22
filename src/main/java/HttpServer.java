9cff4acf18a4edffb46bcf7acec58730d3450b38
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-02-22 11:01:52 +0000

    Server has a port

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -1,11 +1,17 @@
 public class HttpServer {
     private String host;
+    private int port;
 
-    public HttpServer(String host) {
+    public HttpServer(String host, int port) {
         this.host = host;
+        this.port = port;
     }
 
     public String getHost() {
         return host;
+    }
+
+    public int getPort() {
+        return port;
     }
 }
--- src/test/java/HttpServerTest.java
+++ src/test/java/HttpServerTest.java
@@ -1,14 +1,21 @@
-import org.hamcrest.Matchers;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class HttpServerTest {
 
     @Test
     public void serverHasAHost() {
-        HttpServer httpServer = new HttpServer("localhost");
+        HttpServer httpServer = new HttpServer("localhost", 1);
 
-        assertThat(httpServer.getHost(), Matchers.is("localhost"));
+        assertThat(httpServer.getHost(), is("localhost"));
+    }
+
+    @Test
+    public void serverHasAPort() {
+        HttpServer httpServer = new HttpServer("localhost", 8080);
+
+        assertThat(httpServer.getPort(), is(8080));
     }
 }
