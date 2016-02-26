9bdc99395708d732a277754a7ab73a919c204ced
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-02-26 09:19:15 +0000

    Routing criteria updated to use enum rather than string

--- src/test/java/server/RoutingCriteriaTest.java
+++ src/test/java/server/RoutingCriteriaTest.java
@@ -4,13 +4,15 @@ import org.junit.Test;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
+import static server.HttpMethods.GET;
+import static server.HttpMethods.POST;
 
 public class RoutingCriteriaTest {
 
     @Test
     public void routingKeysAreEqual() {
-        RoutingCriteria routingKey1 = new RoutingCriteria("/", "GET");
-        RoutingCriteria routingKey2 = new RoutingCriteria("/", "GET");
+        RoutingCriteria routingKey1 = new RoutingCriteria("/", GET);
+        RoutingCriteria routingKey2 = new RoutingCriteria("/", GET);
 
         assertThat(routingKey1, is(equalTo(routingKey2)));
         assertThat(routingKey1.hashCode(), is(equalTo(routingKey2.hashCode())));
@@ -18,8 +20,8 @@ public class RoutingCriteriaTest {
 
     @Test
     public void routeKeysWithDifferentMethodsNotEqual() {
-        RoutingCriteria routingKey1 = new RoutingCriteria("/", "GET");
-        RoutingCriteria routingKey2 = new RoutingCriteria("/", "POST");
+        RoutingCriteria routingKey1 = new RoutingCriteria("/", GET);
+        RoutingCriteria routingKey2 = new RoutingCriteria("/", POST);
 
         assertThat(routingKey1, not(equalTo(routingKey2)));
         assertThat(routingKey1.hashCode(), not(equalTo(routingKey2.hashCode())));
@@ -27,8 +29,8 @@ public class RoutingCriteriaTest {
 
     @Test
     public void routeKeysWithDifferentRoutesAreNotEqual() {
-        RoutingCriteria routingKey1 = new RoutingCriteria("/another", "GET");
-        RoutingCriteria routingKey2 = new RoutingCriteria("/", "GET");
+        RoutingCriteria routingKey1 = new RoutingCriteria("/another", GET);
+        RoutingCriteria routingKey2 = new RoutingCriteria("/", GET);
 
         assertThat(routingKey1, not(equalTo(routingKey2)));
         assertThat(routingKey1.hashCode(), not(equalTo(routingKey2.hashCode())));
