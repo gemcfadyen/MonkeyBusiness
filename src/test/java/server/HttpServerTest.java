687a728c6439ee7b590f445450449cbbcc67aae3
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-02-29 09:22:44 +0000

    Field variables made local and inlined where appropriate

--- src/test/java/server/HttpServerTest.java
+++ src/test/java/server/HttpServerTest.java
@@ -9,24 +9,19 @@ import static org.junit.Assert.assertThat;
 public class HttpServerTest {
 
     private ServerSocketSpy serverSocketSpy;
-    private RequestParserSpy httpRequestParserSpy;
     private HttpServer httpServer;
-    private ClientSpy clientSpy;
-    private RouteProcessorSpy httpRequestProcessorSpy;
     private ExecutorServiceFactorySpy executorServiceFactorySpy;
     private ExecutorServiceSpy executorServiceSpy;
 
     @Before
     public void setUp() throws Exception {
-        clientSpy = new ClientSpy();
-        serverSocketSpy = new ServerSocketSpy(clientSpy);
-        httpRequestParserSpy = new RequestParserSpy();
-        httpRequestProcessorSpy = new RouteProcessorSpy();
+        serverSocketSpy = new ServerSocketSpy(new ClientSpy());
         executorServiceSpy = new ExecutorServiceSpy();
         executorServiceFactorySpy = new ExecutorServiceFactorySpy(executorServiceSpy);
+
         httpServer = new HttpServer(serverSocketSpy,
-                httpRequestParserSpy,
-                httpRequestProcessorSpy,
+                new RequestParserSpy(),
+                new RouteProcessorSpy(),
                 executorServiceFactorySpy
         );
     }
